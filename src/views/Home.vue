324c20707734125cc0abdb3bb2e30342e7d88153
Leandro Koshoji Araujo <leandro.koshoji@gmail.com>
2021-10-22 01:53:21 -0300

    feat: create home scaffold

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,39 @@
 <template>
-  <div class="home">
-    Home
+  <div class="home" :class="theme">
+    <div class="container">
+      <Searchers :theme="theme"/>
+      Home
+    </div>
   </div>
 </template>
 
 <script>
-
+import Searchers from '../components/Searchers.vue'
 export default {
   name: 'Home',
+  components: { Searchers },
+  props: {
+    theme: {
+            type: String,
+            require: true,
+            default: 'light'
+        }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  min-height: calc(100vh - 80px);
+  padding-top: 3rem;
+
+  &.light {
+    background-color: var(--clr-light-bg);
+    color: var(--clr-light-text);
+  }
+  &.dark {
+    background-color: var(--clr-dark-bg);
+    color: var(--clr-dark-text);
+  }
+}
+</style>
