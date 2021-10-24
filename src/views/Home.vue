b35b9f06290d28620499258a647fa941b938604b
Leandro Koshoji Araujo <leandro.koshoji@gmail.com>
2021-10-24 01:11:29 -0300

    feat: create Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,28 @@
   <div class="home" :class="theme">
     <div class="container">
       <Searchers :theme="theme"/>
-      Home
+      <div class="cards-container">
+        <Card :theme="theme"/>
+        <Card :theme="theme"/>
+        <Card :theme="theme"/>
+        <Card :theme="theme"/>
+        <Card :theme="theme"/>
+        <Card :theme="theme"/>
+        <Card :theme="theme"/>
+        <Card :theme="theme"/>
+        <Card :theme="theme"/>
+        <Card :theme="theme"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Searchers from '../components/Searchers.vue'
+import Card from '../components/Card.vue'
 export default {
   name: 'Home',
-  components: { Searchers },
+  components: { Searchers, Card },
   props: {
     theme: {
             type: String,
@@ -34,6 +46,14 @@ export default {
   &.dark {
     background-color: var(--clr-dark-bg);
     color: var(--clr-dark-text);
+  }
+
+  .cards-container {
+    padding-top: 3rem;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 263px);
+    gap: 76px;
+    justify-content: center;
   }
 }
 </style>
