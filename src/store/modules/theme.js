80392b8ba21f353b139112f2ae329f0e68637347
Leandro Koshoji Araujo <leandro.koshoji@gmail.com>
2021-10-25 00:45:54 -0300

    Update theme.js

--- src/store/modules/theme.js
+++ src/store/modules/theme.js
@@ -1,29 +1,31 @@
 export default {
-    state: {
-        theme: null
+  state: {
+    theme: null,
+  },
+  mutations: {
+    SET_THEME(state, payload) {
+      state.theme = payload;
     },
-    mutations: {
-        SET_THEME(state, payload) {
-            state.theme = payload
-        }
+  },
+  getters: {
+    theme(state) {
+      return state.theme;
     },
-    getters: {
-        theme (state) {
-            return state.theme
-        }
-    },
-    actions: {
-        TOGGLE_THEME ({commit, state}) {
-        const theme = state.theme === 'light' ? 'dark' : 'light'
-        console.log(theme)
-        commit('SET_THEME', theme)
+  },
+  actions: {
+    TOGGLE_THEME({ commit, state }) {
+      const theme = state.theme === "light" ? "dark" : "light";
+      console.log(theme);
+      commit("SET_THEME", theme);
 
-        localStorage.setItem('theme', theme)
-        },
-        SET_INITIAL_THEME ({commit}){
-        const hasThemeStorage = Boolean(localStorage.getItem('theme'))
-        const initialTheme = hasThemeStorage ? localStorage.getItem('theme') : 'light'
-        commit('SET_THEME', initialTheme)
-        }
-    }
-}
+      localStorage.setItem("theme", theme);
+    },
+    SET_INITIAL_THEME({ commit }) {
+      const hasThemeStorage = Boolean(localStorage.getItem("theme"));
+      const initialTheme = hasThemeStorage
+        ? localStorage.getItem("theme")
+        : "light";
+      commit("SET_THEME", initialTheme);
+    },
+  },
+};
